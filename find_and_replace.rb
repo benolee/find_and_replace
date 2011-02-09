595b0d819f7c0a39da25df62f3b39f41467f7b76
Ben Holley <benolee@gmail.com>
2011-02-08 20:50:07 -0600

    added options parser

--- find_and_replace.rb
+++ find_and_replace.rb
@@ -1,17 +1,48 @@
 #!/usr/bin/env ruby
 
-require 'find'
+require 'optparse'
+require 'optparse/time'
+require 'ostruct'
+require 'pp'
 
 class FindAndReplace
-end
 
-if ARGV[0] == '--help'
-  puts "Help"
-# puts "find_and_replace.rb Finds and replaces strings in the given directory."
-# puts "syntax: ruby find_and_replace PATH RegEx"
-  exit
-end
+  #
+  # Return a structure describing the options.
+  #
+  def self.parse(args)
 
-Find.find(ARGV[0]) do |path|
-  # ...
-end
+    # These are the default values
+    options = OpenStruct.new
+    options.verbose = false
+
+    opts = OptionParser.new do |opts|
+      opts.banner = "Usage: example.rb [options]"
+
+      opts.separator ""
+      opts.separator "Specific options:"
+
+      # Boolean switch.
+      opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+        options.verbose = v
+      end
+
+      opts.separator ""
+      opts.separator "Common options:"
+
+      # No argument, shows at tail. This will print an options summary.
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        exit
+      end
+    end
+
+    opts.parse!(args)
+    options
+  end  # parse()
+
+end # class FindAndReplace 
+
+options = FindAndReplace.parse(ARGV)
+pp options
+
